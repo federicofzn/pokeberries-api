5fa5f0fb9726d16f58ed56ba863b6a5fd216ce7d
Federico Forzano <forzanofede@gmail.com>
2024-04-20 19:30:29 -0300

    Feat: added repository file for pokeapi interaction

---
+++ src/pokeberry/infrastructure/pokeberry_repository.py
@@ -0,0 +1,26 @@
+import os
+
+from typing import List
+
+import requests
+
+from src.pokeberry.domain.pokeberry import Pokeberry
+from src.pokeberry.domain.pokeberry_repository import PokeberryRepository
+
+
+class PokeberryApiRepository(PokeberryRepository):
+    """Repository class that interacts with pokeapi."""
+    base_url = os.getenv("POKEAPI_URL")
+
+    def fetch_data(self, url) -> requests.Response:
+        """Fetch data from pokeapi."""
+        return requests.get(url, timeout=2000)
+
+    def get_berries_count(self) -> int:
+        """Get the count of berries from pokeapi."""
+
+    def get_berry(self, berry_id) -> Pokeberry:
+        """Get the berry by id from pokeapi."""
+
+    def get_berries(self, berries_id) -> List[Pokeberry]:
+        """Get the berries by ids from pokeapi."""

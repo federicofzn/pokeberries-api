4bc8a874692d66de9998b14fc4a5e33da7190893
Federico Forzano <forzanofede@gmail.com>
2024-04-20 19:52:39 -0300

    Feat: added Flask restful api and GetAllBerryStats resource

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import json
 from dotenv import load_dotenv
 from flask import Flask, Response
 from flask_injector import FlaskInjector
+from flask_restful import Api
 from injector import Injector
 
+from src.pokeberry.application.get_all_berry_stats import GetAllBerryStats
 from src.pokeberry.infrastructure.pokeberry_dependency_container import PokeberryDependencyContainer
 
 load_dotenv()
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route("/")
@@ -22,6 +25,8 @@ def index():
 
 injector = Injector()
 injector.binder.install(PokeberryDependencyContainer(injector))
+
+api.add_resource(GetAllBerryStats, "/allBerryStats")
 
 FlaskInjector(app=app, injector=injector)
 

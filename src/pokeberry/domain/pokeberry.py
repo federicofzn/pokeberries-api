54897651bd8cc15c7839ee25055ebc27febad72f
Federico Forzano <forzanofede@gmail.com>
2024-04-21 13:05:16 -0300

    Feat: added needed fields to Pokeberry class and to_json method

--- src/pokeberry/domain/pokeberry.py
+++ src/pokeberry/domain/pokeberry.py
@@ -1,2 +1,14 @@
-class Pokeberry(object):
-    pass
+import json
+
+
+class Pokeberry():
+    """Object class with the needed fields for a pokeberry."""
+
+    def __init__(self, id, growth_time, name) -> None:
+        self.id = id
+        self.growth_time = growth_time
+        self.name = name
+
+    def to_json(self):
+        """Returns a json representation of the object."""
+        return json.dumps(self.__dict__)

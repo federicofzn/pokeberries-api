230081c1c3c572e92c3f91e37d2fe9943f4e8e4f
Federico Forzano <forzanofede@gmail.com>
2024-04-21 14:22:23 -0300

    Refactor: change endpoint response to return statistics

--- src/pokeberry/application/get_all_berry_stats.py
+++ src/pokeberry/application/get_all_berry_stats.py
@@ -1,7 +1,9 @@
+import json
 from flask import Response
 from flask_restful import Resource
 from injector import inject
 
+from src.pokeberry.domain.pokeberries_statistics import PokeberriesStatistics
 from src.pokeberry.domain.pokeberry_repository import PokeberryRepository
 
 
@@ -19,8 +21,20 @@ class GetAllBerryStats(Resource):
 
         pokeberries = self.pokeberry_repository.get_berries(berries_ids)
 
+        pokeberries_stats = PokeberriesStatistics(pokeberries)
+
+        response = {
+            "berries_names": [berry.name for berry in pokeberries],
+            "min_growth_time": pokeberries_stats.get_min(),
+            "median_growth_time": pokeberries_stats.get_median(),
+            "max_growth_time": pokeberries_stats.get_max(),
+            "variance_growth_time": pokeberries_stats.get_variance(),
+            "mean_growth_time": pokeberries_stats.get_mean(),
+            "frequency_growth_time": pokeberries_stats.get_frequency()
+        }
+
         return Response(
             mimetype="application/json",
-            response=[pokeberry.to_json() for pokeberry in pokeberries],
+            response=json.dumps(response),
             status=200,
         )

85e1858bd3d8ed9897a843d37ba59f84a8d38557
Federico Forzano <forzanofede@gmail.com>
2024-04-22 15:54:56 -0300

    Refactor: changed graph endpoint to default

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import json
 from dotenv import load_dotenv
-from flask import Flask, Response, render_template
+from flask import Flask, render_template
 from flask_injector import FlaskInjector
 from flask_restful import Api
 from injector import Injector
@@ -16,15 +15,6 @@ api = Api(app)
 
 @app.route("/")
 def index():
-    return Response(
-        mimetype="application/json",
-        response=json.dumps("Welcome to Pokeberries API"),
-        status=200,
-    )
-
-
-@app.route("/graph")
-def graph():
     return render_template("pokeberry/application/graph_template.html")
 
 
